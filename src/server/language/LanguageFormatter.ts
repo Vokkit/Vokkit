7a22e1a50037f0bb8b995a5e4be4a84b8ba70892
Scripter36 <1350adwx@gmail.com>
2019-02-27 15:24:57 +0900

    Fix LanguageFormatter.format not working with empty args

--- src/server/language/LanguageFormatter.ts
+++ src/server/language/LanguageFormatter.ts
@@ -8,14 +8,16 @@ export class LanguageFormatter {
     this.languageName = languageName
   }
 
-  format (key: string, args: { [tag: string]: string }) {
+  format (key: string, args?: { [tag: string]: string }) {
     const languageData = LanguageLoader.load(this.languageName)
     if (!languageData[key]) {
       throw new LanguageKeyNotFoundError(`Cannot find language key: ${key} with language ${this.languageName}`)
     }
     let baseString = languageData[key]
-    for (const tag in args) {
-      baseString = baseString.replace(new RegExp(`\\{${tag}\\}`), args[tag])
+    if (args) {
+      for (const tag in args) {
+        baseString = baseString.replace(new RegExp(`\\{${tag}\\}`), args[tag])
+      }
     }
     return baseString
   }
