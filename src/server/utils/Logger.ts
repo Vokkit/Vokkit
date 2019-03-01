456bfff876eb2a5df6bbb58c1f0124f31a3bc823
Scripter36 <1350adwx@gmail.com>
2019-03-01 17:03:39 +0900

    Add Logger

---
+++ src/server/utils/Logger.ts
@@ -0,0 +1,32 @@
+import readline from 'readline'
+import { ConsoleColor } from './ConsoleColor'
+
+export class Logger {
+  private static clear () {
+    readline.clearLine(process.stdout, 0)
+    readline.cursorTo(process.stdout, 0)
+  }
+
+  private static getTimeString () {
+    return `${ConsoleColor.Bright}${ConsoleColor.FgCyan}${new Date().toISOString()}`
+  }
+
+  private static log (message: any, color: string, prefix: string) {
+    process.stdout.write(`${this.getTimeString()}${color} [${prefix}] ${message}\x1b[0m\n> `)
+  }
+
+  static info (message: any) {
+    this.clear()
+    this.log(message, ConsoleColor.FgWhite, 'INFO')
+  }
+
+  static warn (message: any) {
+    this.clear()
+    this.log(message, ConsoleColor.FgYellow, 'WARN')
+  }
+
+  static error (message: any) {
+    this.clear()
+    this.log(message, ConsoleColor.FgRed, 'ERROR')
+  }
+}
