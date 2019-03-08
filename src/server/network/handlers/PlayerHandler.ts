7b7f0f4c1c1d6dbd534fb369e1b88eff43497a4a
Scripter36 <1350adwx@gmail.com>
2019-03-08 21:08:31 +0900

    Separate PlayerHandler login event

--- src/server/network/handlers/PlayerHandler.ts
+++ src/server/network/handlers/PlayerHandler.ts
@@ -1,6 +1,6 @@
 import { SocketHandler } from './SocketHandler'
-import { Player } from '../../player/Player';
-import { Vokkit } from '../../Vokkit';
+import { Player } from '../../player/Player'
+import { Vokkit } from '../../Vokkit'
 
 export interface PlayerLoginData {
   name: string
@@ -8,28 +8,30 @@ export interface PlayerLoginData {
 
 export class PlayerHandler extends SocketHandler {
   onConnection (socket: SocketIO.Socket) {
-    socket.on('login', (data: PlayerLoginData) => {
-      if (data.name.length === 0) {
-        socket.emit('login_result', { success: false, reason: 'empty_name' })
-        return
+    socket.on('login', (data: PlayerLoginData) => this.onLogin(socket, data))
+  }
+
+  private onLogin (socket: SocketIO.Socket, data: PlayerLoginData) {
+    if (data.name.length === 0) {
+      socket.emit('login_result', { success: false, reason: 'empty_name' })
+      return
+    }
+    const max = Vokkit.getServer().getServerProperties().maximum_name_length
+    if (max && data.name.length > max) {
+      socket.emit('login_result', { success: false, reason: 'long_name' })
+      return
+    }
+    const isInvalidName = Vokkit.getServer().getPlayers().some((player) => {
+      if (player.getName() === data.name) {
+        return true
       }
-      const max = Vokkit.getServer().getServerProperties().maximum_name_length
-      if (max && data.name.length > max) {
-        socket.emit('login_result', { success: false, reason: 'long_name' })
-        return
-      }
-      const isInvalidName = Vokkit.getServer().getPlayers().some((player) => {
-        if (player.getName() === data.name) {
-          return true
-        }
-      })
-      if (isInvalidName) {
-        socket.emit('login_result', { success: false, reason: 'name_already_exist' })
-        return
-      }
-      const player = new Player(socket, data.name)
-      Vokkit.getServer().getPlayers().push(player)
-      socket.emit('login_result', { success: true })
     })
+    if (isInvalidName) {
+      socket.emit('login_result', { success: false, reason: 'name_already_exist' })
+      return
+    }
+    const player = new Player(socket, data.name)
+    Vokkit.getServer().getPlayers().push(player)
+    socket.emit('login_result', { success: true })
   }
 }
