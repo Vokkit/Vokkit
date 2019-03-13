9109b73493a4313fd6b96978e362bb8c5638e7cf
Scripter36 <1350adwx@gmail.com>
2019-03-13 23:28:13 +0900

    Add plugin classes for client

--- src/client/Client.ts
+++ src/client/Client.ts
@@ -1,5 +1,6 @@
 import { LanguageFormatter } from './language/LanguageFormatter'
 import { UserLanguageLoader } from './language/UserLanguageLoader'
+import { PluginManager } from '../server/plugin/PluginManager'
 
 export class Client {
   private language: string
@@ -8,6 +9,8 @@ export class Client {
   init () {
     this.language = UserLanguageLoader.load()
     this.languageFormatter = new LanguageFormatter(this.language)
+
+    PluginManager.loadPlugins()
   }
 
   getLanguageFormatter () {
