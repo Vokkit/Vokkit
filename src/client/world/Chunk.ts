691e902372a76d005da7c935b2e0c3e379d38831
Scripter36 <1350adwx@gmail.com>
2019-03-14 23:52:21 +0900

    Copy world class to client side

---
+++ src/client/world/Chunk.ts
@@ -0,0 +1,58 @@
+import { Position } from '../utils/Position'
+import { Block } from './World'
+
+export interface ChunkPosition {
+  x: number,
+  z: number
+}
+
+export class Chunk {
+  private position: ChunkPosition
+  private blockData: Buffer
+
+  static maxSize = 262144
+
+  constructor (position: ChunkPosition, blockData = Buffer.alloc(Chunk.maxSize)) {
+    this.position = position
+    this.blockData = blockData
+  }
+
+  getBlock (position: Position, checkPosition = true) {
+    if (!checkPosition || this.isPosition(position)) {
+      return this.blockData.readUInt32LE((position.x * 4096 + position.z * 256 + position.y) * 6)
+    }
+  }
+
+  setBlock (newBlock: Block, checkPosition = true) {
+    if (!checkPosition || this.isPosition(newBlock.position)) {
+      this.blockData.writeUInt32LE((newBlock.position.x * 4096 + newBlock.position.z * 256 + newBlock.position.y) * 6, newBlock.id)
+    }
+  }
+
+  getBlockData () {
+    return this.blockData
+  }
+
+  getPosition () {
+    return this.position
+  }
+
+  private isPosition (position: any): position is Position {
+    if (typeof position.x === 'number' && typeof position.y === 'number' && typeof position.z === 'number') {
+      if (position.x >= 0 && position.x < 16 && position.y >= 0 && position.y < 256 && position.z >= 0 && position.z < 16) {
+        return true
+      }
+    }
+    return false
+  }
+
+  checkPosition (position: ChunkPosition) {
+    return position.x >= this.position.x && position.x < this.position.x + 16 && position.z >= this.position.z && position.z < this.position.z + 16
+  }
+
+  getBlockAmount () {
+    let amount = 0
+    for (const id of this.blockData) if (id !== 0) amount++
+    return amount
+  }
+}
---
+++ src/client/world/World.ts
@@ -0,0 +1,49 @@
+import { Chunk, ChunkPosition } from './Chunk'
+import { Position } from '../utils/Position'
+
+export interface Block {
+  position: Position
+  id: number
+}
+
+export class World {
+  private name: string
+  private chunks: Chunk[]
+
+  constructor (name: string, chunks: Chunk[] = []) {
+    this.name = name
+    this.chunks = chunks
+  }
+
+  getName () {
+    return this.name
+  }
+
+  setBlock (newBlock: Block) {
+    for (const chunk of this.chunks) {
+      if (chunk.checkPosition(newBlock.position)) {
+        chunk.setBlock(newBlock)
+      }
+    }
+  }
+
+  getBlock (position: Position) {
+    for (const chunk of this.chunks) {
+      if (chunk.checkPosition(position)) {
+        return chunk.getBlock(position)
+      }
+    }
+  }
+
+  getChunk (position: ChunkPosition) {
+    for (const chunk of this.chunks) {
+      if (chunk.checkPosition(position)) {
+        return chunk
+      }
+    }
+  }
+
+  getChunks () {
+    return this.chunks
+  }
+}
