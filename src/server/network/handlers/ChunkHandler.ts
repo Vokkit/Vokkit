dac7c80cb2ed6ef82462d1ec3dc3cfaebaa2760c
Scripter36 <1350adwx@gmail.com>
2019-03-19 23:03:22 +0900

    Check world and player before server sends chunk

--- src/server/network/handlers/ChunkHandler.ts
+++ src/server/network/handlers/ChunkHandler.ts
@@ -9,13 +9,23 @@ export interface ChunkData {
 
 export class ChunkHandler extends SocketHandler {
   onConnection (socket: SocketIO.Socket) {
-    socket.on('chunk', (data: ChunkData) => this.onChunkLoad(socket, data)) // todo: send chunk without getting request (dangerous)
+    socket.on('chunk', (data: ChunkData) => this.onChunkLoad(socket, data))
   }
 
   onChunkLoad (socket: SocketIO.Socket, data: ChunkData) {
+    const player = Vokkit.getServer().getPlayerById(socket.id)
+    if (!player) {
+      // Todo: check and ban ip
+      return
+    }
     const world = Vokkit.getServer().getWorld(data.worldName)
     if (!world) {
       socket.emit('chunk', { success: false, reason: 'world_name_not_exist' })
+      return
+    }
+    if (player.getLocation().getWorld() !== world) {
+      socket.emit('chunk', { success: false, reason: 'invalid_chunk_request' })
+      return
     }
     const chunk = world.getChunk(data.position)
     socket.emit('chunk', { success: true, chunk: chunk.toJSON(), worldName: data.worldName })
