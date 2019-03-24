e63277a0cd647afd6ea7fe4d9b4859221cf0fcbe
Scripter36 <1350adwx@gmail.com>
2019-03-24 23:28:29 +0900

    Add MainScreen

---
+++ src/client/ui/screens/MainScreen.ts
@@ -0,0 +1,14 @@
+import { Vokkit } from '../../Vokkit'
+import { Screen } from '../Screen'
+
+export class LoginScreen extends Screen {
+  constructor () {
+    super('MainScreen', 'base', null)
+
+    this.init()
+  }
+
+  init () {
+    // TODO
+  }
+}
