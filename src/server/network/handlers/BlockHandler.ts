02d1845abc5d080795f0d03689c2201eee73bc8e
Scripter36 <1350adwx@gmail.com>
2019-03-27 23:17:16 +0900

    Add block break handler

--- src/server/network/handlers/BlockHandler.ts
+++ src/server/network/handlers/BlockHandler.ts
@@ -3,15 +3,20 @@ import { Vokkit } from '../../Vokkit'
 import { NetworkManager } from '../NetworkManager'
 import { LocationObject, Location } from '../../utils/Location'
 import { Block } from '../../block/Block'
+import { Material } from '../../block/Material'
 
 export interface BlockPlaceData {
   location: LocationObject
   id: number
 }
+export interface BlockBreakData {
+  location: LocationObject
+}
 
 export class BlockHandler extends SocketHandler {
   onConnection (socket: SocketIO.Socket) {
     socket.on('block_place', (data: BlockPlaceData) => this.onBlockPlace(socket, data))
+    socket.on('block_break', (data: BlockBreakData) => this.onBlockBreak(socket, data))
   }
 
   onBlockPlace (socket: SocketIO.Socket, data: BlockPlaceData) {
@@ -30,5 +35,23 @@ export class BlockHandler extends SocketHandler {
     }
     location.getWorld().setBlock(location.getPosition(), new Block(data.id))
     NetworkManager.getSocket().emit('set_block', data)
+  }
+
+  onBlockBreak (socket: SocketIO.Socket, data: BlockBreakData) {
+    const player = Vokkit.getServer().getPlayerById(socket.id)
+    if (!player) {
+      // Error: player sent chat without login
+      // Todo: check and ban ip
+      socket.disconnect()
+      return
+    }
+    // todo: hand range check
+    const location = Location.fromObject(data.location)
+    if (location.getWorld().getBlock(location.getPosition()) === 0) {
+      // 공기를 부쉈음
+      return
+    }
+    location.getWorld().setBlock(location.getPosition(), new Block(Material.AIR))
+    NetworkManager.getSocket().emit('break_block', data)
   }
 }
